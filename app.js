24e08cad9bbefc5466abc48877f561d3866aa816
KarenEhab53 <karenehab53@gmail.com>
2026-05-02 12:38:08 +0300

    userschema

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+async function dbConnection() {
+  try {
+    await mongoose.connect(
+      process.env.DB_URL || "mongodb://127.0.0.1:27017/swift_savers",
+    );
+    console.log("DB Connected successfully");
+  } catch (error) {
+    console.error(" DB Connection Error:", error.message);
+  }
+}
+
+const port = process.env.PORT || 3000;
+
+dbConnection();
+app.listen(port, () => {
+  console.log(` Server is running on port ${port}`);
+});
---
+++ models/User.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+
+    email: { type: String, required: true, trim: true, unique: true },
+
+    password: { type: String, required: true, trim: true ,min:6},
+
+    location: {
+      type: String,
+      enum: [
+        "Cairo",
+        "Giza",
+        "Alexandria",
+        "Dakahlia",
+        "Sharqia",
+        "Qalyubia",
+        "Gharbia",
+        "Monufia",
+        "Fayoum",
+        "Beni Suef",
+        "Minya",
+        "Assiut",
+        "Sohag",
+        "Qena",
+        "Luxor",
+        "Aswan",
+        "Suez",
+        "Ismailia",
+        "Port Said",
+        "Red Sea",
+        "North Sinai",
+        "South Sinai",
+      ],
+      default: "Cairo",
+    },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+      match: /^01[0-2,5]{1}[0-9]{8}$/,
+    },
+
+    NID: { type: String, required: true, trim: true, match: /^[0-9]{14}$/ },
+
+    role: {
+      type: String,
+      required: true,
+      enum: ["user", "admin", "doctor"],
+      default: "user",
+    },
+
+    profileImage: {
+      type: String,
+      default: "default.jpg",
+    },
+
+    doctorInfo: {
+      syndicateCardImage: String,
+      universityCertificateImage: String,
+      nationalIdImage: String,
+
+      isApproved: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("User", userSchema);
